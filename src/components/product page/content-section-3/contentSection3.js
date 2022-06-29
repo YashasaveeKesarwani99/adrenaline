2f94115f3ffd30ef9bb46df5db6144c90c4f33eb
YashasaveeKesarwani99 <yashasvikesarwani1@gmail.com>
2022-06-29 11:24:28 +0530

    update

--- src/components/product page/content-section-3/contentSection3.js
+++ src/components/product page/content-section-3/contentSection3.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import "./contentSection3.css";
+import grace from "../../../assets/images/content-section-3-image-1.png";
 
 const contentSection3 = () => {
 	return (
@@ -15,6 +16,38 @@ const contentSection3 = () => {
 						view for attracting the right resources.
 					</div>
 					<div>Overview of the Max Candidate Portal</div>
+				</div>
+				<div className='content-section-3-sub-content-1'>
+					<div>
+						<div>1</div>
+						<div>
+							Personalise the portal to
+							<br /> include your branding
+						</div>
+					</div>
+					<div>
+						<div>2</div>
+						<div>
+							Enable candidates to discover the open positions and set job
+							alerts for preferred positions
+						</div>
+					</div>
+					<div>
+						<div>3</div>
+						<div>
+							Create a qualified candidate funnel using a pre-apply
+							questionnaire
+						</div>
+					</div>
+					<div>
+						<div>4</div>
+						<div>
+							Integrate to external assessment sites for recruitment evaluation
+						</div>
+					</div>
+					<div>
+						<img src={grace} alt='personalized portal' />
+					</div>
 				</div>
 			</div>
 		</div>
